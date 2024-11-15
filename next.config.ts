39e2c468f992035d8b7ab308f2c669d7636dd1f8
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-11-15 20:55:25 +0530

    chages in redirect

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,31 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['ik.imagekit.io', 'cdn0.weddingwire.in'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/weddings-1',
+        destination: '/weddings', // Assuming this is your new correct path
+        permanent: true, // This sends a 308 status code, telling search engines this is a permanent redirect
+      },
+      // Add more old URLs that need to be redirected
+      // {
+      //   source: '/old-path',
+      //   destination: '/new-path',
+      //   permanent: true,
+      // }
+      {
+        source: '/services-1',
+        destination: '/lahza',
+        permanent: true,
+      },
+      {
+        source: '/photographs-1',
+        destination: '/blogs',
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
