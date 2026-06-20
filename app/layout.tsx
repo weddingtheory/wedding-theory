3dbb3f98400beefc63057be01c408cbc7ffa8965
Abhinav Kumar <abhinav.webdj@gmail.com>
2026-06-21 01:27:40 +0530

    update of the clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -137,9 +137,9 @@ export default function RootLayout({
             __html: `
               (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "xa4aqawfnj");
+              })(window, document, "clarity", "script", "xa58gbgkpc");
             `,
           }}
         />
